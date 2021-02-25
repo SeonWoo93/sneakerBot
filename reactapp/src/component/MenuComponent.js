68a82a5ac852a61b1df6beba9f1a73496cb19e6d
SeonWoo93 <dltjsdn93kr@naver.com>
2021-02-25 21:48:47 +0900

    login modal 추가

--- reactapp/src/component/MenuComponent.js
+++ reactapp/src/component/MenuComponent.js
@@ -5,8 +5,30 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Modal from 'react-bootstrap/Modal';
 
 class MenuComponent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            show: false, //modal 초기값(안보임)
+        };
+    }
+
+    //modal close
+    handleClose = () => {
+        this.setState({
+            show: false,
+        });
+    }
+
+    //modal open
+    handleShow = () => {
+        this.setState({
+            show: true,
+        });
+    }
 
     render() {
         const userList = this.props.userList;
@@ -35,10 +57,41 @@ class MenuComponent extends Component {
                     </Nav>
                     <Form inline>
                         <Form.Control type="text" placeholder={userList[0].user_id} readOnly />&nbsp;&nbsp;
-                        <Button variant="outline-light" size="sm">login</Button>&nbsp;&nbsp;
+                        <Button variant="outline-light" size="sm" onClick={this.handleShow}>login</Button>&nbsp;&nbsp;
                         <Button variant="outline-light" size="sm">join</Button>
                     </Form>
                 </Navbar>
+
+                <>
+                    <Modal
+                        show={this.state.show}
+                        onHide={this.handleClose}
+                        backdrop="static"
+                        keyboard={false}
+                    >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Login</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <Form>
+                                <Form.Group controlId="formBasicEmail">
+                                    <Form.Label>user ID</Form.Label>
+                                    <Form.Control type="email" placeholder="sing your ID" />
+                                    <Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" placeholder="sing your Password" />
+                                </Form.Group>
+                            </Form>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="primary" type="submit">submit</Button>
+                            <Button variant="secondary" onClick={this.handleClose}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
+                </>
             </div>
         );
     }
